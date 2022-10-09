dc1c97e1338ec2ce6e7398627f2c9ce0774768ae
MaxDoss3 <90287282+MaxDoss3@users.noreply.github.com>
2022-10-09 16:57:57 -0700

    works, doesnt support error handling

--- Dictionary.cpp
+++ Dictionary.cpp
@@ -16,22 +16,12 @@ private:
     vector<string> userParse;
 
 public:
-
+    //setters and getters
     void setValidKeyword(bool input) {
         validKeyword = input;
     }
-    bool getValidKeyword() {
-        return validKeyword;
-    }
-    void setMapData(map<string, vector<string>> data) {
-        keywordData = data;
-    }
-    map<string, vector<string>> getMapData() {
-        return keywordData;
-    }
-    
-    void setUserParse(string const &input) {
-        
+    void setUserParse(string const& input) {
+
         vector<string> userParsed;
         stringstream in(input);
         string data;
@@ -39,15 +29,22 @@ public:
         while (getline(in, data, ' ')) {
             userParsed.push_back(data);
         }
-      
+
 
         userParse = userParsed;
     }
-    
+    void setMapData(map<string, vector<string>> data) {
+        keywordData = data;
+    }
+    bool getValidKeyword() {
+        return validKeyword;
+    }
+    map<string, vector<string>> getMapData() {
+        return keywordData;
+    }
     vector<string> getUserParse() {
         return userParse;
     }
-
 
 
     //print + userValidation function
@@ -56,7 +53,7 @@ public:
         vector<string> userData = getUserParse();
 
         string keyword, partOfSpeech;
-        bool distinct, reverse, ifValid, validPos;
+        bool distinct, reverse, ifValid, validPos, flagNotPOSDistinct, rFoundInPos;
 
         vector<string> partOfSpeechVec = { "noun", "pronoun", "adjective", "determiner", "verb", "adverb", "preposition", "conjunction", "interjection" };
 
@@ -65,6 +62,8 @@ public:
         validPos = false;
         distinct = false;
         reverse = false;
+        flagNotPOSDistinct = false;
+        rFoundInPos = false;
 
         // if our input is less than 5 slots and the keyword is valid
         if (userData.size() < 5 && ifValid == true) {
@@ -74,54 +73,118 @@ public:
                 keyword = userData[0];
                 
                 if (userData.size() == 1) {
-                    for (map<string, vector<string>>::const_iterator itrer{ data.begin() }; itrer != data.end(); ++itrer) {
-                        vector<string> temp = itrer->second;
+                    for (map<string, vector<string>>::const_iterator itr{ data.begin() }; itr != data.end(); ++itr) {
+                        vector<string> temp = itr->second;
                         for (auto& i : temp) {
-                            cout << keyword << " [" << itrer->first << "]  : " << i << endl;
+                            cout << "     " << keyword << " [" << itr->first << "]  : " << i << endl;
                         }
-                    } // display all pairs
+                    } 
                 }
+                
             }
 
             //if greater than 2 we know keyword is always in 1st position, second postion can contain distinct, however we can have that as a valid POS
             if (userData.size() >= 2) {
                 partOfSpeech = userData[1];
 
-                //check the valid inputs for pos and distinct, set flag for distinct if found in 2nd position.
-                for (int i = 0; i < partOfSpeechVec.size(); i++) {
-                    if (partOfSpeechVec[i] == partOfSpeech || partOfSpeech == "distinct") {
-                        validPos = true;
-                        if (partOfSpeech == "distinct") {
-                            distinct = true;
+                if (userData.size() > 2 && userData.size() < 5) {
+                    if (userData.size() == 3) {
+                        if (userData[2] == "reverse") {
+                            reverse = true;
                         }
-                        else {
-                            distinct = false;
+                        else reverse = false;
+                        
+                        if (userData[2] == "distinct") {
+                            distinct = true;
+                            flagNotPOSDistinct = true;
+                        }
+                        else distinct = false;
+                    }
+                    else if (userData.size() == 4) {
+
+                        if (userData[2] == "distinct") {
+                            distinct = true;
+                            flagNotPOSDistinct = true;
+                        }
+                        else distinct = false;
+
+                        if (userData[2] == "reverse") {
+                            reverse = true;
+                        }else if (userData[3] == "reverse"){
+                            reverse = true;
                         }
                     }
                 }
 
-                if (validPos == true && distinct == false && reverse == false){
-                    
-                    for (auto itr = data.find(userData[1]); itr != data.end(); itr++) {
-                        if (itr->first == userData[1]) {
-                            vector<string> temp = itr->second;
-                            cout << "FOUND INFO: Key | POS |" << endl;
-                            for (auto& i : temp) {
-                                    
-                                cout << keyword << " [" << itr->first << "]  : " << i << endl;
+                //check the valid inputs for pos and distinct, set flag for distinct if found in 2nd position.
+                for (int i = 0; i < partOfSpeechVec.size(); i++) {
+                    if (partOfSpeechVec[i] == partOfSpeech || partOfSpeech == "distinct" || partOfSpeech == "reverse") {
+                        validPos = true;
+                        if (userData.size() == 2) {
 
+                            if (partOfSpeech == "reverse") {
+                                reverse = true;
+                                rFoundInPos = true;
+                            }
+                            else
+                                reverse = false;
+
+
+                            if (partOfSpeech == "distinct") {
+                                distinct = true;
+                            }
+                            else
+                                distinct = false;
+                        }
+                    }
+                }
+
+                ///handling POS + keyword for 2 + reverse
+                if (validPos == true && distinct == false){
+                    if (reverse == false) {
+                        for (auto itr = data.find(userData[1]); itr != data.end(); itr++) {
+
+                            if (itr->first == userData[1]) {
+                                vector<string> temp = itr->second;
+
+                                for (auto& i : temp) {
+                                    cout << "     " << keyword << " [" << itr->first << "]  : " << i << endl;
+                                }
                             }
                         }
                     }
-                    cout << "status of distinct : " << distinct << endl;
+                    else {
+                        if (rFoundInPos) {
+                            for (map<string, vector<string>>::reverse_iterator itr{ data.rbegin() }; itr != data.rend(); ++itr) {
+                                vector<string> temp = itr->second;
+                                for (auto& i : temp) {
+                                    cout << "     " << keyword << " [" << itr->first << "]  : " << i << endl;
+                                }
+                            }
+                        }
+                        else {
+                            for (auto itr = data.find(userData[1]); itr != data.end(); itr++) {
+
+                                if (itr->first == userData[1]) {
+                                    vector<string> temp = itr->second;
+
+                                    for (auto i = temp.rbegin(); i != temp.rend(); i++) {
+                                        cout << "     " << keyword << " [" << itr->first << "]  : " << *i << endl;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                        
                 }
                 
+                ///handling distinct + keyword via a map + reversed
                 else if (validPos == true && distinct) {
 
-                    for (map<string, vector<string>>::const_iterator itrerer{ data.begin() }; itrerer != data.end(); ++itrerer) {
+                    for (map<string, vector<string>>::const_iterator itr{ data.begin() }; itr != data.end(); ++itr) {
                         map<string, string> keepTrack;
                         
-                        vector<string> temp = itrerer->second;
+                        vector<string> temp = itr->second;
                         for (int i = 0; i < temp.size(); i++) {
                             string tempString = temp[i];
                             //to lower in order to make sure an capitalization conflicts on the definition are parsed out
@@ -130,20 +193,53 @@ public:
                                 c = tolower(c);
                             });
 
-                            keepTrack.emplace(tempString, itrerer->first);
+                            keepTrack.emplace(tempString, itr->first);
                         }
                        
-                        for (map<string, string>::const_iterator itrererer{ keepTrack.begin() }; itrererer != keepTrack.end(); ++itrererer) {
-                            cout << keyword << " [" << itrererer->second << "]  :" << itrererer->first << endl;
+                        if (reverse == false) {
+                            
+                            if (flagNotPOSDistinct) {
+                                for (map<string, string>::const_iterator itr2{ keepTrack.begin() }; itr2 != keepTrack.end(); ++itr2) {
+                                    if (itr2->second == partOfSpeech) {
+                                        cout << "     " << keyword << " [" << itr2->second << "]  :" << itr2->first << endl;
+                                    }
+                                }
+                               
+                            }
+                            else {
+                                for (map<string, string>::const_iterator itr2{ keepTrack.begin() }; itr2 != keepTrack.end(); ++itr2) {
+                                    cout << "     " << keyword << " [" << itr2->second << "]  :" << itr2->first << endl;
+                                }
+                            }
                         }
+                        if (reverse == true) {
+                            if (flagNotPOSDistinct) {
+                                for (map<string, string>::reverse_iterator itr3{ keepTrack.rbegin() }; itr3 != keepTrack.rend(); ++itr3) {
+                                    if (itr3->second == partOfSpeech) {
+                                        cout << "     " << keyword << " [" << itr3->second << "]  :" << itr3->first << endl;
+                                    }
+                                }
+
+                            }
+                            else {
+                                for (map<string, string>::reverse_iterator itr3{ keepTrack.rbegin() }; itr3 != keepTrack.rend(); ++itr3) {
+                                    cout << "     " << keyword << " [" << itr3->second << "]  :" << itr3->first << endl;
+                                }
+                            }
+                            
+                        }
+                        
                     }
                 }
-                cout << endl;
             }
         }
-
+        if (!getValidKeyword() == false) {
+            cout << "    |" << endl;
+        }
+        
     }
-
+    
+    //create a map of Part of speech to a vector of definitions
     void createMap(vector<string> partOfSpeech, vector<string> definition) {
         map <string, vector<string>> posDef;
         vector <string> definitionsCollection;
@@ -190,6 +286,7 @@ public:
         setMapData(posDef);
     }
 
+    //parse out all data after key
     void findAllDef(string line) {
         vector<string> definitions;
         vector<string> partOSpeech;
@@ -226,6 +323,7 @@ public:
         createMap(partOSpeech, definitions);
     }
 
+    //get the key
     void findKey(vector<string> data, string userIn) {
         vector<string> parsed;
         setValidKeyword(true);
@@ -252,20 +350,21 @@ public:
 
         }
         if (!keyFound) {
-            cout << "Sorry there is no valid keyword entered, to be considered" << endl;
+            cout << "     <NOT FOUND> To be considered for the next release. Thank you" << endl << "    |" << endl;
+            cout << "    |" << endl;
+            cout << "     PARAMETER HOW-TO, please enter:" << endl;
+            cout << "     1. A search key - then 2. An optional part of speech - then" << endl
+                << "     3. An optional 'distinct' -then 4. An optional 'reverse'" << endl << "    |" << endl;
             setValidKeyword(false);
         }
     }
-
-
-    
 };
 
 int main() {
     Dictionary dictionary;
     vector<string> data;
     int count = 1;
-    bool exit = false;
+    int keyCount = 0;
     string userInput = "";
     fstream ioFile;
     bool end = false;
@@ -275,30 +374,40 @@ int main() {
     if (ioFile.is_open()) {
         string temp;
         while (getline(ioFile, temp)) {
+            keyCount++;
             data.push_back(temp);
         }
     }
 
     cout << "! Loading Complete...\n" << endl << "===== DICTIONARY 340 JAVA =====" <<
-       endl << "----- Keywords: " << endl << "----- Definitions: " << endl;
+       endl << "----- Keywords: " << keyCount << endl << "----- Definitions: " << endl;
 
     while (!end) {
         
         
-        cout << "enter entry: ";
+        cout << "Search["<< count << "]: ";
         getline(cin, userInput);
-
+        
+        
+        count++;
         //convert user input to lower
         for_each(userInput.begin(), userInput.end(), [](char& c) {
             c = tolower(c);
         });
 
 
-        if (userInput == "end") {
+        if (userInput == "!q") {
             end = true;
+            cout << endl << "-----THANK YOU-----";
+        }
+        else if (userInput == " " || userInput == "!help" || userInput == "") {
+            cout << "    |" << endl;
+            cout << "     PARAMETER HOW-TO, please enter:" << endl;
+            cout << "     1. A search key - then 2. An optional part of speech - then" << endl 
+                << "     3. An optional 'distinct' -then 4. An optional 'reverse'" << endl << "    |" << endl;
         }
         else {
-
+            cout << "    |" << endl;
             dictionary.findKey(data, userInput);
             map<string, vector<string>> keywordData = dictionary.getMapData();
             dictionary.toString(userInput, keywordData);
@@ -307,32 +416,3 @@ int main() {
 
      return 0;
 }
-
-
-
-///parsing function which looks for keyword, pos, definition
-///itterate through vector ds to find keyword, parse keyword and definition
-///placing into map
-
-///map to hold pos and defin
-
-
-///output parsed data
-
-///distinct functionality, parse user input for distinct keyword
-///use distinct unordered map for unique output of definitions
-
-///reverse function, reverse the vector 
-
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
